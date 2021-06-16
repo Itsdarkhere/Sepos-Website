9c19837ab1ab66b6cebe6d486a90fbaa1e4064e9
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2021-06-16 20:57:22 +0300

    fontwork mostly

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,7 +1,12 @@
 import './Footer.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Footer() {
+
+    const [policy, togglePolicy] = useState(true)
+    const policyToggle = () => {
+        togglePolicy(!policy)
+    }
 
     return (
         <footer className="bottom-outer-container">
@@ -9,13 +14,20 @@ export default function Footer() {
                 <button className="footer-logo-button" onClick={() => window.scrollTo(0, 0)}>
                     <p>SEPOS OY</p>
                 </button>
+                {policy ? 
                 <div className="footer-tags-container">
-                    <div className="footer-policies">
-                        <div className="policy">Privacy Policy</div>
-                        <div className="policy">Terms of Use</div>
+                    <div className="footer-policies" onClick={() => policyToggle()}>
+                        <button className="policy" 
+                        onClick={() => policyToggle()}>Privacy Policy</button>
                     </div>
                     <div className="footer-befound">@BeFound Web Design</div>
                 </div>
+                :
+                <div className="policy-component">
+                    <div className="policy-info">We do not gather, keep or use any of your data.</div>
+                    <button className="policy-close" onClick={() => policyToggle()}>Close</button>
+                </div>
+                }
             </div>
         </footer>
     )
